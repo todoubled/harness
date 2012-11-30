795ea0f8570b352241c24fe4bdf302e8bd77bfe6
Todd Larsen <tlarsen@groupon.com>
2012-11-29 18:34:40 -0800

    Generated documentation for f2212fb1aa7032f474b2e8f428b2be1ac7b1e4da

--- assets/behavior.js
+++ assets/behavior.js
@@ -306,7 +306,34 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "title": "Casper.js Integration Test Helpers",
             "slug": "casperjs-integration-test-helpers"
           },
-          "depth": 2
+          "depth": 2,
+          "children": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 5,
+                "title": "Get just the value segment of the cookie.",
+                "slug": "get-just-the-value-segment-of-the-cookie"
+              },
+              "depth": 5
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 5,
+                "title": "Clear cookies before the test run starts to avoid pollution.",
+                "slug": "clear-cookies-before-the-test-run-starts-to-avoid-pollution"
+              },
+              "depth": 5
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 5,
+                "title": "Reset the URL hash after each test run completes to avoid pollution.",
+                "slug": "reset-the-url-hash-after-each-test-run-completes-to-avoid-pollution"
+              },
+              "depth": 5
+            }
+          ]
         }
       ]
     }, {
