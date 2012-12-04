e1f27e3ca552c074ba154c7be70521abacb36b9d
Todd Larsen <tlarsen@groupon.com>
2012-12-03 18:18:48 -0800

    env docs

--- harness/env.sh
+++ harness/env.sh
@@ -1,6 +1,7 @@
 # ## Environment Variables
 #
 # ###### Configure scripts with custom paths and other settings.
+# ---
 #
 # - `PROJECT_PREFIX` is used for globbing on deployment and generating built asset filenames.
 # - `CASPER_HELPERS` augments the helpers in `harness/casper-helpers.coffee` with your own for more succint integration tests.
