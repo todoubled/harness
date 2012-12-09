f81aa911e95b170108c7a152c9459f39a44b7ea9
Todd Larsen <tlarsen@groupon.com>
2012-12-09 10:28:54 -0800

    Generated documentation for 54931539e66b84f6448bf81c131f43765d084a31

--- assets/behavior.js
+++ assets/behavior.js
@@ -98,18 +98,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                 "title": "HTML",
                 "slug": "html"
               },
-              "depth": 2,
-              "children": [
-                {
-                  "type": "heading",
-                  "data": {
-                    "level": 4,
-                    "title": "Implementation",
-                    "slug": "implementation"
-                  },
-                  "depth": 4
-                }
-              ]
+              "depth": 2
             }, {
               "type": "heading",
               "data": {
@@ -172,18 +161,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "title": "HTML",
             "slug": "html"
           },
-          "depth": 2,
-          "children": [
-            {
-              "type": "heading",
-              "data": {
-                "level": 4,
-                "title": "Implementation",
-                "slug": "implementation"
-              },
-              "depth": 4
-            }
-          ]
+          "depth": 2
         }, {
           "type": "heading",
           "data": {
